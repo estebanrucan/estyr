cfc3e85945be2185f494ac812a515567704f513d
estebanrucan <errucan@uc.cl>
2021-04-12 13:00:15 -0400

    Algunos errores

--- R/crear_proyecto.R
+++ R/crear_proyecto.R
@@ -29,7 +29,7 @@ crear_proyecto <- function(estructura = TRUE) {
 
     is_a_project <- length(grep("\\.Rproj$", list.files(dir))) > 0
 
-    stopifnot(is_a_project)
+    if (is_a_project) stop('Already a project')
 
     setwd(dir)
 
